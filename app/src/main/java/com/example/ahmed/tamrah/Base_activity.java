b04b16476f253af8102aef5bf24710650aef6987
Ahmed AlQarni <a7med-2300@hotmail.com>
2018-03-09 04:37:38 +0300

    1-Fixed search Bar 2-Middle title Bar

---
+++ app/src/main/java/com/example/ahmed/tamrah/Base_activity.java
@@ -0,0 +1,52 @@
+package com.example.ahmed.tamrah;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
+
+
+public class Base_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public void setContentView(int layoutResID)
+    {
+        DrawerLayout fullView = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_base_activity, null);
+        FrameLayout activityContainer = (FrameLayout) fullView.findViewById(R.id.flContents);
+
+        getLayoutInflater().inflate(layoutResID, activityContainer, true);
+
+        super.setContentView(fullView);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (useToolbar())
+        {
+            setSupportActionBar(toolbar);
+        }
+        else
+        {
+            toolbar.setVisibility(View.GONE);
+        }
+    }
+
+    protected boolean useToolbar()
+    {
+        return true;
+    }
+
+}
