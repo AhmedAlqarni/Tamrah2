39a72e07291cac049d37784dbc3885322650f2d8
Abdulrahman <s201324890@kfupm.edu.sa>
2018-03-11 18:46:02 +0300

    Added Khalid's changes in User.java

--- app/src/main/java/com/example/ahmed/tamrah/User.java
+++ app/src/main/java/com/example/ahmed/tamrah/User.java
@@ -17,10 +17,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import java.util.concurrent.Executor;
+import java.util.HashMap;
+import java.util.Map;
 
-import butterknife.internal.ListenerClass;
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by khalidalnamlah on 3/9/18.
@@ -34,37 +40,43 @@ public class User {
     private String address;
     private Image profileImage;
     private Context context;
-    private ProgressDialog progressDialog ;
+    private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
     private FirebaseUser user;
+    private FirebaseAuth.AuthStateListener firebaseAuthListner;
+    private DatabaseReference databaseReference;
+    private FirebaseDatabase firebaseDatabase;
+    private Map<String, Object> x;
 
-    public User(Context context){
+
+    public User(Context context) {
         this.context = context;
         progressDialog = new ProgressDialog(context);
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
 
-        if(user != null){
+        if (user != null) {
             Log.i("1-", "user is null");
         }
     }
 
-    public void register(String email, String password){
-        if(TextUtils.isEmpty(email)){
+    public void register(String email, String password) {
+        if (TextUtils.isEmpty(email)) {
             //email is empty
             Toast.makeText(context.getApplicationContext(), "Please enter your email ", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if(TextUtils.isEmpty(password)){
+        if (TextUtils.isEmpty(password)) {
             //password is empty
             Toast.makeText(context.getApplicationContext(), "Please enter your email ", Toast.LENGTH_SHORT).show();
         }
         firebaseAuth = FirebaseAuth.getInstance();
         progressDialog.setMessage("Registering ...");
         progressDialog.show();
-        firebaseAuth.createUserWithEmailAndPassword(email,password)
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener((Activity) context, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) { //CASTING
@@ -73,8 +85,7 @@ public class User {
                             Toast.makeText(context.getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
                             // Later
                             //loginAfterRegister(email,password);
-                        }
-                        else{
+                        } else {
                             progressDialog.dismiss();
                             AlertDialog alertDialog = new AlertDialog.Builder(context).create();
                             alertDialog.setTitle("Registering Failed");
@@ -92,31 +103,30 @@ public class User {
 
     }
 
-    public void login(String email, String password){
+    public void login(String email, String password) {
 
-        if(TextUtils.isEmpty(email)){
+        if (TextUtils.isEmpty(email)) {
             //email is empty
             Toast.makeText(context.getApplicationContext(), "Please enter your email ", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if(TextUtils.isEmpty(password)){
+        if (TextUtils.isEmpty(password)) {
             //password is empty
             Toast.makeText(context.getApplicationContext(), "Please enter your email ", Toast.LENGTH_SHORT).show();
         }
         firebaseAuth = FirebaseAuth.getInstance();
         progressDialog.setMessage("Logging in ...");
         progressDialog.show();
-        firebaseAuth.signInWithEmailAndPassword(email,password)
+        firebaseAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener((Activity) context, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         progressDialog.dismiss();
-                        if(task.isSuccessful()){ // if logging was successfull (from firebase)
-                            ((Activity)context).finish();
-                            context.startActivity(new Intent(context.getApplicationContext(),MainActivity.class));
-                        }
-                        else{
+                        if (task.isSuccessful()) { // if logging was successfull (from firebase)
+                            ((Activity) context).finish();
+                            context.startActivity(new Intent(context.getApplicationContext(), MainActivity.class));
+                        } else {
                             AlertDialog alertDialog = new AlertDialog.Builder(context).create();
                             alertDialog.setTitle("Logging Failed");
                             alertDialog.setMessage("Invalid email or password");
@@ -127,17 +137,17 @@ public class User {
                                         }
                                     });
                             alertDialog.show();
-                            Log.i("g","faile"); // failure in logging in
+                            Log.i("g", "faile"); // failure in logging in
                         }
                     }
                 });
 
     }
 
-    public void logout(){
+    public void logout() {
         AlertDialog alertDialog = new AlertDialog.Builder(context).create();
         alertDialog.setTitle("Logout");
-        alertDialog.setMessage("You are signed in as: "+ user.getEmail());
+        alertDialog.setMessage("You are signed in as: " + user.getEmail());
         alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Logout",
                 new DialogInterface.OnClickListener() {
                     //firebase logout
@@ -145,11 +155,11 @@ public class User {
                         //firebaseAuth = FirebaseAuth.getInstance();
                         firebaseAuth.signOut();
                         dialog.dismiss();
-                        if(user ==null){
-                            Log.i("1","right");
+                        if (user == null) {
+                            Log.i("1", "right");
                         }
                         //startActivity(new Intent(this,MainActivity.class));
-                        Log.i("1","logged out");
+                        Log.i("1", "logged out");
                         Toast.makeText(context.getApplicationContext(), "LoggedOut", Toast.LENGTH_LONG).show();
                         //mDrawerLayout.closeDrawers();
                     }
@@ -164,6 +174,113 @@ public class User {
         alertDialog.show();
 
     }
+
+
+    public void getUserProfile(){
+        /*
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            //boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            String uid = user.getUid();
+        }
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            name = user.getDisplayName();
+            Log.i("kkk",name);
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            String uid = user.getUid();
+        } */
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("User");//.child("name");//what is the argument
+        //databaseReference.
+
+
+
+        //To be Reviewed ...
+        databaseReference.addValueEventListener(new ValueEventListener() {
+
+            //This will be called when there is a change in the User child
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot childSnapshot: dataSnapshot.getChildren()) {
+                    x = (Map<String, Object>) childSnapshot.getValue();
+                    //Log.i("1",);
+                    name = x.get("name").toString();
+                    region = x.get("region").toString();
+                    description = x.get("description").toString();
+                    phoneNum = x.get("phoneNum").toString();
+                    profileImage = ((Image) x.get("profileImage"));
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public void updateProfile(){
+
+        String userId = firebaseAuth.getCurrentUser().getUid();
+        DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("User").child(userId);
+        Map newPost = new HashMap();
+        newPost.put("name","khalid");
+        newPost.put("phoneNum","966565684101");
+        newPost.put("region","Riyadh");
+        newPost.put("description","Fuck you MotherFucker...");
+        newPost.put("profileImage","http://justfood.nawa3em.com/subimg/106376015231.jpg");
+
+        current_user_db.setValue(newPost);
+        /*
+        Log.i("g","kljdglksjgl;js;ljgsdg");
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName("Khalid")
+                .setPhotoUri(Uri.parse("https://i2.wp.com/www.alhadathnews.net/wp-content/uploads/2013/09/f4h.net_1.jpg?resize=615%2C297"))
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User profile updated.");
+                        }
+                        else {
+                            Log.i("1","faliled");
+                        }
+                    }
+                });
+        getUserProfile();*/
+    }
+
+
     /*
     public void  loginAfterRegister(String email, String password){ // After the user has registered, he will be signed in automatically
         //firebaseAuth
@@ -177,6 +294,53 @@ public class User {
                 });
 
     }*/
+
+    public void confirmEmail(){
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        user.sendEmailVerification()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
+                        }
+                    }
+                });
+    }
+
+    public void resetPassword(String emailAddress){
+        firebaseAuth = FirebaseAuth.getInstance();
+        //String emailAddress = "user@example.com";
+
+        firebaseAuth.sendPasswordResetEmail(emailAddress)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
+                        }
+                    }
+                });
+
+    }
+
+    public void setPassword(String newPassword){
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        //String newPassword = "SOME-SECURE-PASSWORD";
+
+        user.updatePassword(newPassword)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User password updated.");
+                        }
+                    }
+                });
+    }
 
 
 }
